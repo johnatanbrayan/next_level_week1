019652bb8c37c71a822344242e63ce24d936902c
johnatanbrayan <johnatanbrayan1@gmail.com>
2020-06-07 12:56:08 -0300

    Organizacao do codigo

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,14 +9,14 @@ const routes = express.Router();
 // Lists todos os itens
 routes.get('/itens', itensController.index);
 
-// Cria e Populariza os points
-routes.post('/points', pointsController.create);
-
 // Lista todos os points
 routes.get('/points', pointsController.index);
     
 // Retorna um point especifico
 routes.get('/points/:id', pointsController.show);
+
+// Cria e Populariza os points
+routes.post('/points', pointsController.create);
 
 /* ------Anotacao(Vale lembrar):
     Boas praticas de nomeacao dos metodos no padrao api restfull
